173f1e5fb029855507dac99d96b382248d049380
Matthew Yee <myee@redhat.com>
2026-02-26 13:00:08 -0800

    Update setup scripts for RHEL3 to include installation script and remove tmux removal command
    - Added a command to download and execute an installation script from lmstudio.ai in the `setup-automation/setup-rhel3.sh` file.
    - Removed the command to uninstall tmux from the `setup-automation/setup-rhel3.sh` file.
    - Commented out the certificate download and podman run commands in the `runtime-automation/01-introduction/setup-rhel3.sh` file.

--- runtime-automation/01-introduction/setup-rhel3.sh
+++ runtime-automation/01-introduction/setup-rhel3.sh
@@ -2,4 +2,3 @@
 
 #curl -k -O /root/katello-server-ca.crt https://satellite.lab/pub/katello-server-ca.crt
 #podman run --publish 8080:8080 --volume /root/katello-server-ca.crt:/app/ca.pem:ro,Z   registry.redhat.io/satellite/foreman-mcp-server-rhel9:6.18 --foreman-url https://satellite.lab --no-verify-ssl &
-curl -fsSL https://lmstudio.ai/install.sh | bash
--- setup-automation/setup-rhel3.sh
+++ setup-automation/setup-rhel3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-dnf remove -y tmux
 systemctl stop dnf-automatic-install.timer
 systemctl disable dnf-automatic-install.timer
 systemctl mask dnf-automatic-install.timer
 
+curl -fsSL https://lmstudio.ai/install.sh | bash
